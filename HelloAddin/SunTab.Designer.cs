42748aaa4436e9af6399e54a8f3c766d07b1aafb
Artur Sienicki <artur.sienicki@live.com>
2016-07-09 15:57:25 +0200

    T -> Outlook i addin 1/2: https://sienicki.org/outlook-i-addin-1-z-2/

---
+++ HelloAddin/SunTab.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace HelloAddin
+{
+    partial class SunTab : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public SunTab()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.weatherTab = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.weatherTab.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // weatherTab
+            // 
+            this.weatherTab.Groups.Add(this.group1);
+            this.weatherTab.Label = "Pogoda";
+            this.weatherTab.Name = "weatherTab";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.button1);
+            this.group1.Label = "Pogodynka";
+            this.group1.Name = "group1";
+            // 
+            // button1
+            // 
+            this.button1.Image = global::HelloAddin.Properties.Resources.sun;
+            this.button1.Label = "Sprawdź pogodę";
+            this.button1.Name = "button1";
+            this.button1.ShowImage = true;
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnCheckWeather);
+            // 
+            // SunTab
+            // 
+            this.Name = "SunTab";
+            this.RibbonType = "Microsoft.Outlook.Appointment, Microsoft.Outlook.MeetingRequest.Send";
+            this.Tabs.Add(this.weatherTab);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.SunTab_Load);
+            this.weatherTab.ResumeLayout(false);
+            this.weatherTab.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab weatherTab;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal SunTab SunTab
+        {
+            get { return this.GetRibbon<SunTab>(); }
+        }
+    }
+}
---
+++ HelloAddin/SunTab.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace HelloAddin
+{
+    public partial class SunTab
+    {
+        private void SunTab_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void btnCheckWeather(object sender, RibbonControlEventArgs e)
+        {
+            MessageBox.Show("Jutro znowu gorąco! Kup klimę albo będziesz zdychał :)");
+        }
+    }
+}
